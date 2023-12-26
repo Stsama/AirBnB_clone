5f712e320e7c72a647f1e000c6b94883b83a02c7
Stsama <albertezoua98@gmail.com>
2023-12-26 09:17:38 -0800

    task 6 done

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import cmd
+class HBNBCommand(cmd.Cmd):
+    """Represents a command class"""
+
+    prompt = "(hbnb)"
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program"""
+        print("")
+        return True
+    def emptyline(self):
+        """an empty line + ENTER shouldn’t execute anything"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
